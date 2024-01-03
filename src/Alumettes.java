11a98d3e93b05903204d3abe1423c91e94504807
xAm64 <48089283+xAm64@users.noreply.github.com>
2024-01-03 15:04:11 +0100

    Ajout du jeu de base

--- src/Alumettes.java
+++ src/Alumettes.java
@@ -1,9 +1,52 @@
+import java.util.Scanner;
 
 public class Alumettes {
 
 	public static void main(String[] args) {
+		int numbersAlumettes = 21, numberRest = 21;
+		String replay = "non";
+		Scanner scn = new Scanner(System.in);
 		System.out.println("Exercice 2 Les alumettes");
-		int numbersAlumettes = 21;
+		System.out.println("voici les alumettes "+viewAllumettes(numbersAlumettes, numbersAlumettes));
+		System.out.println("1joueur ou 2 joueurs ?");
+		String numberPlayerString = scn.nextLine();
+		do {
+			numberRest = numbersAlumettes;
+			if (verifInt(numberPlayerString)) {
+				if (Integer.parseInt(numberPlayerString) == 1) {
+					System.out.println("Joueur contre ordinateur");
+				} else if (Integer.parseInt(numberPlayerString) == 2) {
+					System.out.println("Joueur contre joueur");
+				} else {
+					System.out.println("Merci de saisir 1 joueur ou 2 joueurs");
+				}
+			} else {
+				System.out.println("Appuyer sur 1 pour jouer contre l'ordinateur ou 2 pour jouer à 2");
+			}
+			System.out.println("Recommencer ?");
+			replay = scn.nextLine();
+		}while (replay.equals("oui"));
+		System.out.println("Au revoir");
 	}
-
+	
+	private static boolean verifInt (String x) {
+		Scanner scn = new Scanner(x);
+		boolean out = false;
+		if (scn.hasNextInt()) {
+			out = true;
+		}
+		return out;
+	}
+	
+	private static String viewAllumettes(int numbersAlumettes, int numberTotalAlumettes) {
+		String out = "";
+		for (int i=0;i<numberTotalAlumettes;i++){
+			if (i<numbersAlumettes) {
+				out += "¡";
+			} else {
+				out += "·";
+			}
+		}
+		return out;
+	}
 }
